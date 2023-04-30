0560a7f9e04ddf779d2ded5a5986640dfd8690bb
kikka <clovers.4th@gmail.com>
2023-05-01 00:04:46 +0900

    ルートパラメータに対するモデルの自動取得までの分をまとめてコミット

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminBlogController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,5 +24,10 @@ Route::get('/contact', [ContactController::class,'index'])->name('contact');
 Route::post('/contact',[ContactController::class,'sendMail']);
 Route::get('/contact/complete', [ContactController::class,'complete'])->name('contact.complete');
 
-//make:bladeでbladeを作成するコマンド
-Route::view('/hello', 'hello');
+//ブログ
+Route::get('/admin/blogs', [AdminBlogController::class,'index'])->name('admin.blogs.index');
+Route::get('/admin/blogs/create', [AdminBlogController::class,'create'])->name('admin.blogs.create');
+Route::post('/admin/blogs',[AdminBlogController::class,'store'])->name('admin.blogs.store');
+Route::get('/admin/blogs/{blog}',[AdminBlogController::class,'edit'])->name('admin.blogs.edit');
+Route::put('/admin/blogs/{blog}',[AdminBlogController::class,'update'])->name('admin.blogs.update');
+Route::delete('/admin/blogs/{blog}',[AdminBlogController::class,'destroy'])->name('admin.blogs.destroy');
